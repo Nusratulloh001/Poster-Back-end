4d9a54c15fd11507b75c49b992b005dc295699de
Nusratulloh001 <77152595+Nusratulloh001@users.noreply.github.com>
2022-08-18 22:37:58 +0500

    This is first practical work [Back-end for Poster].

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request -> validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+        
+        Post::create([
+            'title' => $request -> input('title'),
+            'content' => $request -> input('content'),
+        ]);
+        return response([
+            'msg' => 'Post created'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $post -> update([
+            'title' => $request -> input('title'),
+            'content' => $request -> input('content'),
+        ]);
+        $post -> save();
+        return response([
+            'msg' => 'Post updated'
+        ], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post -> delete();
+        return response([], 204);
+    }
+}
